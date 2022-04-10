a7d89982f64b0246d2dd04034b44aacf95540b53
Diogo <up202006059@up.pt>
2022-04-11 00:59:57 +0100

    put all html and css of restaurants

---
+++ project/casual.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <title>Hasburgui</title>    
+    <meta charset="UTF-8">
+    <link href="css/casual_style.css" rel="stylesheet">
+    <link href="css/page_layout.css" rel="stylesheet">
+  </head>
+  <body>
+  <header>
+        <div id="logo">
+        <img src="images/hasburgi2.png" alt="hasburgi logo">
+        </div>
+      <div id="signup">
+        <button class="button">Signup</button>
+      </div>
+    </header>
+    <div id="searchbar">
+        <input type="text" placeholder="Search Food" class="searchbar">
+    </div>
+    <h2>Restaurants :</h2>
+      <section id="foodsections">
+        <article>
+          <img src="https://picsum.photos/418/190">
+          <a href="restaurant.php?id=1">Restaurant name</a>
+        </article>
+        <article>
+          <img src="https://picsum.photos/418/190">
+          <a href="restaurant.php?id=1">Restaurant name</a>
+        </article>
+        <article>
+          <img src="https://picsum.photos/418/190">
+          <a href="restaurant.php?id=1">Restaurant name</a>
+        </article>
+        <article>
+          <img src="https://picsum.photos/418/190">
+          <a href="restaurant.php?id=1">Restaurant name</a>
+        </article>
+        <article>
+          <img src="https://picsum.photos/418/190">
+          <a href="restaurant.php?id=1">Restaurant name</a>
+        </article>
+      </section>
+    <footer>
+      <p>&copy; Hasburgui Industries, 2022</p>
+    </footer>
+  </body>
+</html>
---
+++ project/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en-US">
+  <head>
+    <title>Hasburgui</title>    
+    <meta charset="UTF-8">
+    <link href="css/index_style.css" rel="stylesheet">
+    <link href="css/index_layout.css" rel="stylesheet">
+  </head>
+  <body>
+  <header>
+        <div id="logo">
+        <img src="images/hasburgi2.png" alt="hasburgi logo">
+        </div>
+      <div id="signup">
+        <button class="button">Signup</button>
+      </div>
+    </header>
+    <div id="searchbar">
+        <input type="text" placeholder="Search Food" class="searchbar">
+      </div>
+    <section id="foodsections">
+      <article><img src="images/fast.png" alt=""></article>
+      <article><img src="images/gourmet.png" alt=""></article>
+      <article><img src="images/healthy.png" alt=""></article>
+      <article><img src="images/desserts.png" alt=""></article>
+    </section>
+    <footer>
+      <p>&copy; Hasburgui Industries, 2022</p>
+    </footer>
+  </body>
+</html>
