5b58d0eb2dbc9b3968dd33a42c8c36b01799ca5d
Diogo <up202006059@up.pt>
2022-05-23 23:23:37 +0100

    added profile's style and layout

--- project/template/common.tpl.php
+++ project/template/common.tpl.php
@@ -55,8 +55,8 @@
     <title>Hasburgui</title>    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <link href="css/index_style.css" rel="stylesheet">
-    <link href="css/page_layout.css" rel="stylesheet">
+    <link href="css/profile_style.css" rel="stylesheet">
+    <link href="css/profile_layout.css" rel="stylesheet">
   </head>
   <body>
   <header>
@@ -72,6 +72,9 @@
       <a href="index.php">
         <button class="button">Back</button>
       </a>
+    </div>
+    <div id="logout">
+      <button class="button">Logout</button>
     </div>
     </header>
 
