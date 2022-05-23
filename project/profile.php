d4a1a35171cf7626faa83f9647e930d4e5fe45f3
Diogo <up202006059@up.pt>
2022-05-23 22:25:50 +0100

    made html and some php of profile page

---
+++ project/profile.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types = 1);
+
+require_once('util/session.php');
+require_once('template/common.tpl.php');
+$session = new Session();
+
+drawHeaderProfile($session);
+?>
+  <div id="userInfo">
+    <h1>Account Information</h1>
+    <h2>Name: <?=$session->getName();?></h2>
+    <h2>Phone: 999999999</h2>
+    <h2>Address: </h2>
+    <ul> 
+      <li>City: </li>
+      <li>Postal Code: </li>
+      <li>Street: </li>
+    </ul>
+  </div>
+
+<?php drawFooter() ?>
