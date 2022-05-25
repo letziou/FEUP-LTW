11f3c4aa1cb1c380acd8db7af3e7a7aa4f11709a
CarlosMealha <up202005954@edu.fc.up.pt>
2022-05-25 19:07:01 +0100

    I'm trying to adjust the body size grid in Register and Login pages

--- project/register.php
+++ project/register.php
@@ -4,8 +4,7 @@
     <title>Hasburgui</title>    
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-    <link href="css/login_style.css" rel="stylesheet">
-    <link href="css/login_layout.css" rel="stylesheet">
+    <link href="css/register_layout.css" rel="stylesheet">
     <link href="css/register_style.css" rel="stylesheet">
     <link href="css/page_layout.css" rel="stylesheet">
   </head>
