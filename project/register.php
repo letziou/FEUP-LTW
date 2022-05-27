a66b9a172088272c134ab6cc09b8883bb6dc83f8
CarlosMealha <up202005954@edu.fc.up.pt>
2022-05-27 16:20:41 +0100

    added some css changes on page layout

--- project/register.php
+++ project/register.php
@@ -22,7 +22,7 @@
   <div id="loginbox">
   <h2> Register </h2>
     <form action="action_register.php" id="formreg" method="post" class="register">
-      <input type="text" name="username" placeholder="username">
+      <input type="text" name="email" placeholder="email">
         <br>
       <input type="password" name="password" placeholder="password">
         <br>
@@ -36,7 +36,7 @@
         <span id=buttons>
       <button class="register_button" form="formreg">Register</button>
       <a href="login.php">
-      <button class="login_button">Login Here</button>
+      <button class="login_button">Login</button>
       </a>
 </span>
 </div>
