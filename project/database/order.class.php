00189a0229bd6f558f3d5c1e1956815fba2cd2eb
JDThreepwood <93098803+JDThreepwood@users.noreply.github.com>
2022-06-13 19:31:13 +0100

    added order functions

--- project/database/order.class.php
+++ project/database/order.class.php
@@ -5,19 +5,50 @@
     public int $id_Order;
     public float $price;
     public string $description;
+    public string $order_status;
     public int $id_Restaurant;
     public int $id_User;
 
-    public function __construct(int $id_Order, float $price, string $description, int $id_Restaurant, int $id_User)
+    public function __construct(int $id_Order, float $price, string $description, string $order_status int $id_Restaurant, int $id_User)
     {
       $this->id_Order = $id_Order;
       $this->price = $price;
       $this->description = $description;
+      $this->order_status = $order_status;
       $this->id_Restaurant = $id_Restaurant;
       $this->id_User = $id_User;
     }
 
-    /*static function getRestaurantOrders(PDO $db, int $id_Restaurant) : Order {}*/
+    static function save_newOrder(PDO $db, float $price, string $description, string $order_status, int $id_Restaurant, int $id_User) {
+      $stmt = $db->prepare('INSERT INTO Order (price, description, order_status, id_Restaurant, id_User) 
+                            VALUES (?,?,?,?,?)');
+
+      $stmt->execute(array($price, $description, $order_status, $id_Restaurant, $id_User));
+
+    }
+
+    static function getOrdersFromRestaurant(PDO $db, int $id_Restaurant) : array {
+        $stmt = $db->prepare('SELECT id_Order, price, description, order_status, id_Restaurant, id_User
+                              FROM Order 
+                              WHERE id_Restaurant= ?');
+        $stmt->execute(array($id_Restaurant));      
+    
+        $orders = array();
+        while ($order = $stmt->fetch()) {
+          $orders[] = new Order(
+            (int)$order['id_Order'],
+            (float)$order['price'],
+            (string)$order['description'],
+            (string)$order['order_status'],
+            (int)$order['id_Restaurant'],
+            (int)$order['id_User']
+            
+          );
+        }
+    
+        return $orders;
+      }
+    }
       
 }
 
