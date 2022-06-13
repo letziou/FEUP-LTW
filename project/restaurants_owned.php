1a5de8f0c792b96491316a8922ffabd353a95abe
Diogo <diogoalmeida.vlc@gmail.com>
2022-06-13 19:39:50 +0100

    something

---
+++ project/restaurants_owned.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types =1);
+
+require_once('util/session.php');
+$session = new Session();
+
+require_once('database/connection.db.php');
+require_once('database/restaurant.class.php');
+
+require_once('template/common.tpl.php');
+require_once('template/restaurant.tpl.php');
+
+  $db= getDatabaseConnection();
+
+
+  $restaurants = Restaurant::getRestaurants($db, intval($_GET['id']));
+
+
+  drawHeaderProfile($session);
+  drawFooter();
+
+?>
