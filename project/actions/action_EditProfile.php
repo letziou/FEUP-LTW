f8046496a57bbabf1d9dc3086e0171ffd8dc34f6
JDThreepwood <93098803+JDThreepwood@users.noreply.github.com>
2022-06-14 15:19:56 +0100

    edit  Profile working

---
+++ project/actions/action_EditProfile.php
@@ -0,0 +1,68 @@
+<?php
+  declare(strict_types = 1);
+
+  require_once(__DIR__ . '/../util/session.php');
+  $session = new Session();
+
+  require_once(__DIR__ . '/../database/connection.db.php');
+  require_once(__DIR__ . '/../database/user.class.php');
+  require_once(__DIR__ . '/../database/address.class.php');
+
+  $db = getDatabaseConnection();
+
+  $user = User::getUser($db, $session->getId_User());
+  $address = Address::getAddressFromID($db, (int)$user->id_Address);
+
+    $email_new = $_POST['email'];
+    $password_new = $_POST['password'];
+    $password2_new = $_POST['confirm_password'];
+    $street_new = $_POST['street'];
+    $city_new = $_POST['city'];
+    $postalCode_new = $_POST['postalCode'];
+    $phone_new = $_POST['phone'];  
+
+    if (!empty($_POST['email'])) {
+        $user->email = $email_new;
+    }
+
+    if (!empty($_POST['password'])) {
+        if($password_new != $password2_new ){
+            $session->addMessage('Error','Passwords do not match');
+            header('Location: /../edit_profile.php');
+        }
+        else{
+            $user->password = $password_new;
+        }
+    }
+
+    if (!empty($_POST['phone'])) {
+        $user->phone = (int)$phone_new;
+    }
+
+    if (!empty($_POST['street'])) {
+        $address->street = $street_new;
+    }
+
+    if (!empty($_POST['city'])) {
+        $address->city = $city_new;
+    }
+
+    if (!empty($_POST['postalCode'])) {
+        $address->postalCode = $postalCode_new;
+    }
+
+
+    $address->save_AddressEdit($db);
+    
+    $user->save_editUser($db);
+    
+    $newUser = User:: getUser($db, (int)$id_User);
+
+    if ($user) {
+        //$session->setId_User($user->id_User);
+        //$session->setName($user->fullName());
+        $session->addMessage('Success', 'Account Edited');
+        header('Location: /../profile.php');
+      } 
+
+?>
--- project/template/profile.tpl.php
+++ project/template/profile.tpl.php
@@ -13,10 +13,39 @@
       <li>Postal Code: <?=$address->postalCode?> </li>
       <li>Street: <?=$address->street?></li>
     </ul>
+    <span id=buttons>
+    <a href="edit_profile.php">
+    <button class="edit_button"> Edit Profile</button>
+  </span> 
   </div>
   <div class="restButton">
     <a href="restaurants_owned.php?id=<?=$user->id_User?>">
     <button>Checkout your restaurants!</button>
   </a>
   </div>
+<?php } ?>
+
+<?php function drawEditProfile(Session $session, User $user, Address $address) { ?>
+  <div id="loginbox">
+  <h2> Edit Profile </h2>
+    <form action="/actions/action_EditProfile.php" id="formedit" method="post" class="edit">
+      <input id="email" type="text" name="email" placeholder="e-mail">
+        <br>
+      <input id="password" type="password" name="password" placeholder="Password">
+        <br>
+      <input id="confirm_password" type="confirm_password" name="confirm_password" placeholder="Confirm Password">
+        <br>
+      <input  id="street" type="text" name="street" placeholder="Street">
+        <br>
+      <input  id="city" type="text" name="city" placeholder="City">
+        <br>
+      <input  id="postalCode" type="text" name="postalCode" placeholder="Postal Code">
+        <br>
+      <input id="phone" type="tel" name="phone" placeholder="Phone Number">
+        <br>
+        </form>
+        <span id=buttons>
+      <button class="edit_button" form="formedit">Confirm Changes</button>
+      </span>
+</div>
 <?php } ?>
